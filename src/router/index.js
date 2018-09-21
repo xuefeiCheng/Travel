0b08d26162824149dec5cc53cd6a775119d38b05
程雪飞 <1805711033@qq.com>
2018-09-21 16:32:29 +0800

    vue-router index.js 设置默认路径

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,14 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+  // esLint检测 空格 key前2space value前4space 不使用tab
+  // 设置默认路由
     {
       path: '/',
+      redirect: '/hello'
+    },
+    {
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
