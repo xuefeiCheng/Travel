bd91700c3e7094ece76cb3268196f7fd7898a51c
程雪飞 <1805711033@qq.com>
2018-09-22 12:23:29 +0800

    code of project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
@@ -8,14 +8,14 @@ export default new Router({
   routes: [
   // esLint检测 空格 key前2space value前4space 不使用tab
   // 设置默认路由
+    // {
+    //   path: '/',
+    //   redirect: '/hello'
+    // },
     {
       path: '/',
-      redirect: '/hello'
-    },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
