3d65a85ee49c605ba6cce844b6e8c838a050441a
程雪飞 <1805711033@qq.com>
2018-09-23 17:30:14 +0800

    icons组件 样式微调

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -90,33 +90,35 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    position: relative
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    float: left
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      box-sizing: border-box
-      padding: .1rem
-      bottom: .44rem
-      .icon-img-content
-        display: block
-        height: 100%
-        margin: 0 auto
-    .icon-img-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $markTextColor
-      padding:0 .1rem
-      ellipsis()
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      float: left
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        box-sizing: border-box
+        padding: .1rem
+        bottom: .44rem
+        .icon-img-content
+          display: block
+          height: 100%
+          margin: 0 auto
+      .icon-img-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $markTextColor
+        padding:0 .1rem
+        ellipsis()
 </style>
