217004350c766a47d9257955b5987899b31c0de5
程雪飞 <1805711033@qq.com>
2018-09-24 15:40:06 +0800

    add alphabet component

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <li class="item">a</li>
+    <li class="item">b</li>
+    <li class="item">c</li>
+    <li class="item">d</li>
+    <li class="item">e</li>
+    <li class="item">f</li>
+    <li class="item">g</li>
+    <li class="item">h</li>
+    <li class="item">i</li>
+    <li class="item">j</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/variables.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      text-align: center
+      line-height: .4rem
+      color: $bgColor
+</style>
