0adf6ce6edb10e1376d61016de3b7a51ed3eae9b
程雪飞 <1805711033@qq.com>
2018-09-24 13:54:56 +0800

    city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/variables.styl'
+  .search
+    height: .72rem
+    background-color: $bgColor
+    padding: 0 .1rem
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      text-align: center
+      color: #666
+      border-radius: .06rem
+      height: .62rem
+      line-height: .62rem
+      padding: 0 .1rem
+</style>
