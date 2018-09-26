4f62d36394670a9ed81c47d3b5d1d76e45f80ebd
程雪飞 <1805711033@qq.com>
2018-09-26 21:49:59 +0800

    search logic finished

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,72 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="输入城市名或拼音"
+        v-model="keyword"
+      />
+    </div>
+    <div class="search-list" ref="search" v-show='keyword'>
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show='hasData'>未查询到有效数控</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +85,18 @@ export default {
       height: .62rem
       line-height: .62rem
       padding: 0 .1rem
+  .search-list
+    overflow: hidden
+    position: absolute
+    z-index: 1
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #ccc
+    .search-item
+      line-height: .62rem
+      color: #666
+      background: #fff
+      padding-left: .2rem
 </style>
